6904b3f96eeb192a4825e95573986dac3f6ca02c
VOSAICA <2190796788@qq.com>
2020-09-13 16:35:02 +0800

    prepare for test

---
+++ convert.py
@@ -0,0 +1,27 @@
+from scipy.spatial.transform import Rotation as R
+
+groundtruth = open(
+    '/home/raoshi/Documents/datasets/KITTI_odometry_poses/10.txt', 'r')
+converted = open("groundtruth.txt", 'w')
+temp = groundtruth.readline()
+i = 0
+while True:
+    i += 1
+    temp = groundtruth.readline()
+    temp = temp.split()
+    R11 = float(temp[0])
+    R12 = float(temp[1])
+    R13 = float(temp[2])
+    x = float(temp[3])
+    R21 = float(temp[4])
+    R22 = float(temp[5])
+    R23 = float(temp[6])
+    y = float(temp[7])
+    R31 = float(temp[8])
+    R32 = float(temp[9])
+    R33 = float(temp[10])
+    z = float(temp[11])
+    r = R.from_matrix([[R11, R12, R13], [R21, R22, R23], [R31, R32, R33]])
+    r = r.as_quat()
+    qx, qy, qz, qw = r[0], r[1], r[2], r[3]
+    converted.write('%f %f %f %f %f %f %f %f\n' % (i, x, y, z, qx, qy, qz, qw))
--- test.py
+++ test.py
@@ -2,38 +2,49 @@ import numpy as np
 import cv2
 
 from visual_odometry import PinholeCamera, VisualOdometry
+from scipy.spatial.transform import Rotation as R
 
-cam = PinholeCamera(1241.0, 376.0, 718.8560, 718.8560, 607.1928, 185.2157)
+cam = PinholeCamera(1226.0, 370.0, 718.8560, 718.8560, 607.1928, 185.2157)
 vo = VisualOdometry(
-    cam, '/home/raoshi/Documents/datasets/KITTI_odometry_poses/00.txt')
+    cam, '/home/raoshi/Documents/datasets/KITTI_odometry_poses/10.txt')
 
-traj = np.zeros((600, 600, 3), dtype=np.uint8)
+traj = np.zeros((1200, 1200, 3), dtype=np.uint8)
 
-for img_id in range(4541):
-    img = cv2.imread(
-        '/home/raoshi/Documents/datasets/KITTI_odometry_color/00/image_2/' +
-        str(img_id).zfill(6) + '.png', 0)
+for img_id in range(10000):
+    try:
+        img = cv2.imread(
+            '/home/raoshi/Documents/datasets/KITTI_odometry_color/10/image_2/'
+            + str(img_id).zfill(6) + '.png', cv2.IMREAD_GRAYSCALE)
+        vo.update(img, img_id)
 
-    vo.update(img, img_id)
+        cur_t = vo.cur_t
+        cur_r = vo.cur_R
+        r = R.from_matrix(cur_r)
+        r = r.as_quat()
+        qx, qy, qz, qw = r[0], r[1], r[2], r[3]
+        i = img_id
+        if (img_id > 2):
+            x, y, z = cur_t[0], cur_t[1], cur_t[2]
+        else:
+            x, y, z = 0., 0., 0.
+        draw_x, draw_y = int(x) + 290, int(z) + 90
+        with open("results.txt", 'a') as outposeF:
+            outposeF.write('%f %f %f %f %f %f %f %f\n' %
+                           (i, x, y, z, qx, qy, qz, qw))
+        true_x, true_y = int(vo.trueX) + 290, int(vo.trueZ) + 90
 
-    cur_t = vo.cur_t
-    if (img_id > 2):
-        x, y, z = cur_t[0], cur_t[1], cur_t[2]
-    else:
-        x, y, z = 0., 0., 0.
-    draw_x, draw_y = int(x) + 290, int(z) + 90
-    true_x, true_y = int(vo.trueX) + 290, int(vo.trueZ) + 90
+        cv2.circle(traj, (draw_x, draw_y), 1,
+                   (img_id * 255 / 4540, 255 - img_id * 255 / 4540, 0), 1)
+        cv2.circle(traj, (true_x, true_y), 1, (0, 0, 255), 2)
+        cv2.rectangle(traj, (10, 20), (600, 60), (0, 0, 0), -1)
+        text = "Coordinates: x=%2fm y=%2fm z=%2fm" % (x, y, z)
+        cv2.putText(traj, text, (20, 40), cv2.FONT_HERSHEY_PLAIN, 1,
+                    (255, 255, 255), 1, 8)
 
-    cv2.circle(traj, (draw_x, draw_y), 1,
-               (img_id * 255 / 4540, 255 - img_id * 255 / 4540, 0), 1)
-    cv2.circle(traj, (true_x, true_y), 1, (0, 0, 255), 2)
-    cv2.rectangle(traj, (10, 20), (600, 60), (0, 0, 0), -1)
-    text = "Coordinates: x=%2fm y=%2fm z=%2fm" % (x, y, z)
-    cv2.putText(traj, text, (20, 40), cv2.FONT_HERSHEY_PLAIN, 1,
-                (255, 255, 255), 1, 8)
+        cv2.imshow('Road facing camera', img)
+        cv2.imshow('Trajectory', traj)
+        cv2.waitKey(1)
 
-    cv2.imshow('Road facing camera', img)
-    cv2.imshow('Trajectory', traj)
-    cv2.waitKey(1)
-
-cv2.imwrite('map.png', traj)
+        cv2.imwrite('map.png', traj)
+    except:
+        cv2.imwrite('map.png', traj)
